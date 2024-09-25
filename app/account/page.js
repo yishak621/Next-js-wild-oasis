9b64fc40c8b741f3788fba03ef675a7ac2f8a2fa
yishak621 <yishakabrham621@gmail.com>
2024-09-25 16:07:58 +0300

    new

--- app/account/page.js
+++ app/account/page.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 export const metadata = {
@@ -5,7 +6,19 @@ export const metadata = {
 };
 
 function Page() {
-  return <div>account page</div>;
+  return (
+    <div>
+      <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+        Welcome , Jonas
+      </h2>
+      <p className="text-lg">
+        You have no reservations yet. Check out our{" "}
+        <Link className="underline text-accent-500" href="/cabins">
+          luxury cabins &rarr;
+        </Link>
+      </p>
+    </div>
+  );
 }
 
 export default Page;
