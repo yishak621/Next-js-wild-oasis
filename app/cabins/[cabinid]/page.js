0c371c8dbbc80d1659297e2b2cc3383e8e251b3e
yishak621 <yishakabrham621@gmail.com>
2024-09-30 21:21:32 +0300

    new-files

--- app/cabins/[cabinid]/page.js
+++ app/cabins/[cabinid]/page.js
@@ -1,17 +1,31 @@
 import { getCabin } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+
+//dynamic metadata
+export async function generateMetadata({ params }) {
+  const { name } = await getCabin(params.cabinid);
+  return {
+    title: `Cabin ${name} `,
+  };
+}
 
 export default async function Page({ params }) {
   const cabin = await getCabin(params.cabinid);
-  console.log(cabin);
-  const { name, maxCapacity, regularPrice, discount, image, description } =
+
+  const { id, name, maxCapacity, regularPrice, discount, image, description } =
     cabin;
 
   return (
     <div className="max-w-6xl mx-auto mt-8">
       <div className="grid grid-cols-[3fr_4fr] gap-20 border border-primary-800 py-3 px-10 mb-24">
         <div className="relative scale-[1.15] -translate-x-3">
-          <img src={image} alt={`Cabin ${name}`} />
+          <Image
+            src={image}
+            alt={`Cabin ${name}`}
+            fill
+            className=" object-cover"
+          />
         </div>
 
         <div>
